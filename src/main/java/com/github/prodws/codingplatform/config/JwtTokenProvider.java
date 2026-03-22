595386ebacdeb44525f161734e0634621b6e3551
prodws <169688005+prodws@users.noreply.github.com>
2026-03-22 09:46:19 +0100

    fix: specify algorithm

--- src/main/java/com/github/prodws/codingplatform/config/JwtTokenProvider.java
+++ src/main/java/com/github/prodws/codingplatform/config/JwtTokenProvider.java
@@ -3,6 +3,8 @@ package com.github.prodws.codingplatform.config;
 import com.github.prodws.codingplatform.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -22,6 +24,8 @@ public class JwtTokenProvider {
     public String generateToken(User user) {
         Instant now = Instant.now();
 
+        JwsHeader header = JwsHeader.with(MacAlgorithm.HS256).build();
+
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .subject(String.valueOf(user.getId()))
                 .claim("email", user.getEmail())
@@ -29,6 +33,6 @@ public class JwtTokenProvider {
                 .expiresAt(now.plusMillis(expiration))
                 .build();
 
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        return jwtEncoder.encode(JwtEncoderParameters.from(header, claims)).getTokenValue();
     }
 }
