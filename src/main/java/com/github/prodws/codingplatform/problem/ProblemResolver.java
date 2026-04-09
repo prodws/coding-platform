f9436feb09368f2c841850ffc8c4f067abac4060
prodws <169688005+prodws@users.noreply.github.com>
2026-04-09 20:57:15 +0200

    feat: add ProblemResolver and createProblem mutation with input types

---
+++ src/main/java/com/github/prodws/codingplatform/problem/ProblemResolver.java
@@ -0,0 +1,37 @@
+package com.github.prodws.codingplatform.problem;
+
+import com.github.prodws.codingplatform.problem.registration.CreateProblemRequest;
+import com.github.prodws.codingplatform.problem.registration.FileInput;
+import com.github.prodws.codingplatform.problem.registration.OptionInput;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ProblemResolver {
+
+    private final ProblemService problemService;
+
+    @QueryMapping
+    public Problem problemByTitle(@Argument String title) {
+        return problemService.getProblemByTitle(title);
+    }
+
+    // TODO: restrict createProblem to ADMIN role only
+    @MutationMapping
+    public Problem createProblem(
+            @Argument ProblemType type,
+            @Argument String title,
+            @Argument ProblemDifficulty difficulty,
+            @Argument List<FileInput> files,
+            @Argument List<OptionInput> options
+    ) {
+        CreateProblemRequest request = new CreateProblemRequest(type, title, difficulty, files, options);
+        return problemService.createProblem(request);
+    }
+}
